d24712d15a17fe8e64c793db3354d99e2750c123
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-05-25 22:00:55 +0530

    Time: 239 ms (48.85%), Space: 10 MB (60.35%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,49 @@
+#define ll long long
+
+class Solution {
+public:
+    
+    bool call(vector<int> &a,vector<ll> &ans,ll &t,int i)
+    {
+        int n = a.size();
+        if(i==n)
+        {
+            if(ans[0]==t&&ans[0]==ans[1]&&ans[0]==ans[2]&&ans[0]==ans[3])
+                return 1;
+            return 0;
+        }
+        
+        for(int j=0;j<4;j++)
+        {
+            if(ans[j]+a[i]<=t){
+            ans[j] += a[i];
+            if(call(a,ans,t,i+1))
+                return 1;
+            ans[j]-=a[i];
+            }
+        }
+        return 0;
+        
+    }
+    
+    bool makesquare(vector<int>& a) {
+        
+        int n = a.size();
+        long long sum =0;
+        for(auto i:a)
+            sum += i;
+        if(sum%4!=0)
+            return 0;
+        int mx = *max_element(a.begin(),a.end());
+        long long t= sum/4;
+        if(mx>t)
+            return 0;
+        sort(a.begin(),a.end(),greater<int> ());
+        vector<ll> ans(4,0);
+        
+        return call(a,ans,t,0);
+        
+        
+        
+    }
+};
