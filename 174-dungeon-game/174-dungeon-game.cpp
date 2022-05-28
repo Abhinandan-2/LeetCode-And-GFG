9a0beacfc5ebf76f8bd38ae91d43730bac3b7bce
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-05-28 13:15:43 +0530

    Time: 9 ms (53.47%), Space: 8.6 MB (99.29%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -5,36 +5,20 @@ public:
         int n = a.size() , m = a[0].size(),h;
         
         if(a[n-1][m-1]>0)
-            a[n-1][m-1] =1;
+            a[n-1][m-1] =0;
         
         for(int i=n-2;i>=0;i--)
         {
-            if(a[i+1][m-1]==1)
-            {
-                if(a[i][m-1]>0)
-                    a[i][m-1]=1;
-            }
-            else
-            {
-                a[i][m-1] += a[i+1][m-1];
-                if(a[i][m-1]>0)
-                    a[i][m-1]=1;
-            }
+            a[i][m-1] += a[i+1][m-1];
+            if(a[i][m-1]>0)
+                a[i][m-1]=0;
         }
         
         for(int j=m-2;j>=0;j--)
         {
-            if(a[n-1][j+1]==1)
-            {
-                if(a[n-1][j]>0)
-                    a[n-1][j]=1;
-            }
-            else
-            {
-                a[n-1][j] += a[n-1][j+1];
-                if(a[n-1][j]>0)
-                    a[n-1][j]=1;
-            }
+            a[n-1][j] += a[n-1][j+1];
+            if(a[n-1][j]>0)
+                a[n-1][j]=0;
         }
         
         for(int i=n-2;i>=0;i--)
@@ -42,21 +26,12 @@ public:
             for(int j=m-2;j>=0;j--)
             {
                 h = max(a[i+1][j],a[i][j+1]);
-                if(h==1)
-                {
-                    if(a[i][j]>0)
-                        a[i][j]=1;
-                }
-                else
-                {
-                    a[i][j] += h;
-                    if(a[i][j]>0)
-                        a[i][j] = 1;
-                }
+                a[i][j] += h;
+                if(a[i][j]>0)
+                    a[i][j] = 0;
             }
         }
-        if(a[0][0]==1)
-            return 1;
+       
         return abs(a[0][0])+1;
         
         
