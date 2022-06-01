914cfa3a289ee1b7e877920fc8237208f1e54392
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-01 14:25:24 +0530

    Time: 617 ms (91.22%), Space: 95.6 MB (74.32%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -10,27 +10,33 @@ public:
         
         n = a.size();
         m = a[0].size();
-        int sum ,ans =0;
+        int ans =0,h,k1;
         for(int i=1;i<n;i++)
             for(int j=1;j<m;j++)
                 a[i][j] += (a[i-1][j]+a[i][j-1]-a[i-1][j-1]);
         
-        for(int i=1;i<n;i++)
+        // for(auto i:a)
+        // {
+        //     for(auto j:i)
+        //         cout<<j<<" ";
+        //     cout<<endl;
+        // }
+        
+        unordered_map<int,int> mp;
+        for(int i=1;i<m;i++)
         {
-            for(int j=1;j<m;j++)
+            for(int j=i;j<m;j++)
             {
-                for(int x=1;x<=i;x++)
+                mp[0] = 1;
+                for(int k=1;k<n;k++)
                 {
-                    for(int y=1;y<=j;y++)
-                    {
-                        sum = a[i][j];
-                        sum -= a[x-1][j];
-                        sum -= a[i][y-1];
-                        sum += a[x-1][y-1];
-                        if(sum==t)
-                            ans++;
-                    }
+                    h = a[k][j]-a[k][i-1];
+                    k1 = h-t;
+                    if(mp.count(k1))
+                        ans += mp[k1];
+                    mp[h]++;
                 }
+                mp.clear();
             }
         }
         
