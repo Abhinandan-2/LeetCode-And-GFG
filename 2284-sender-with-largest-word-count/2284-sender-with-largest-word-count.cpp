cb048793d51070f47a437a139bb6620820f84ce2
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-03 13:25:49 +0530

    Time: 384 ms (52.17%), Space: 77.9 MB (28.60%) - LeetHub

---
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    
+    string call(string a,string b)
+    {
+        int n = a.size() , m = b.size();
+        
+        int i=0,j=0;
+        
+        while(i<n&&j<m)
+        {
+            if(a[i]<b[i])
+                return b;
+            if(a[i]>b[i])
+                return a;
+            i++;
+            j++;
+        }
+        if(i==n)
+            return b;
+        return a;
+        
+    }
+    
+    string largestWordCount(vector<string>& a, vector<string>& b) {
+        
+        int n = a.size();
+        unordered_map<string,int> m;
+        string p,h;
+        int c1;
+        for(int i=0;i<n;i++)
+        {
+            c1=0;
+            p = a[i];
+            stringstream c(p);
+            while(getline(c,h,' '))
+                c1++;
+            m[b[i]] += c1;
+        }
+        string ans;
+        int mx =0 ;
+        for(auto i:m)
+        {
+            if(i.second>=mx)
+            {
+                if(i.second>mx)
+                {
+                    ans = i.first;
+                }
+                mx = i.second;
+                ans = call(i.first,ans);
+            }
+        }
+        
+        return ans;
+        
+        
+        
+    }
+};
