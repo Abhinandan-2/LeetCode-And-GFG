e36444a047a0e7001db2cf3e3ff8148c4acb7e26
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-08 00:59:52 +0530

    Time: 593 ms (57.03%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select employee_id, if((mod(employee_id,2)=1 and substr(name,1,1)!= 'M') ,salary , 0 ) as bonus
-# case
-#     when mod(salary,2) =1 and substr(name,1) != 'M' then salary 
-#     else 0 
-# end
+select employee_id,
+case
+    when (mod(employee_id,2) =1 and substr(name,1,1) != 'M') then salary 
+    else 0 
+end as bonus
 from employees
