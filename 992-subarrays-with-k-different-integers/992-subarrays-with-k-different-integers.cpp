7da4c704021a0eb7d8544a34db347cefd7fe0120
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-10 13:01:51 +0530

    Time: 285 ms (24.26%), Space: 64.4 MB (5.19%) - LeetHub

---
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,101 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& a, int k) {
+        
+        list<int> q;
+        unordered_map<int,int> v;
+        unordered_map<int,list<int> ::iterator> m;
+        int n = a.size() , st =0 ,i=0,ans=0,h;
+        while(m.size()!=k&&i<n)
+        {
+            q.push_back(i);
+            auto it = q.end();
+            it--;
+            if(m.count(a[i]))
+            {
+                auto ij = m[a[i]];
+                q.erase(ij);
+            }
+            m[a[i]] = it;
+            v[a[i]]++;
+            i++;
+        }
+        if(m.size()!=k)
+            return 0;
+        ans += (q.front()-st+1);
+        // cout<<i<<" "<<st<<" "<<ans<<" "<<q.front()<<endl;
+        while(i<n)
+        {
+            q.push_back(i);
+            auto it = q.end();
+            it--;
+            if(m.count(a[i]))
+            {
+                auto ij = m[a[i]];
+                q.erase(ij);
+            }
+            else
+            {
+                h = q.front();
+                q.pop_front();
+                m.erase(a[h]);
+            }
+            m[a[i]] = it;
+            v[a[i]]++;
+            while(v.size()>k)
+            {
+                // if(m.count)
+                v[a[st]]--;
+                if(v[a[st]]==0)
+                    v.erase(a[st]);
+                st++;
+            }
+            ans += q.front()-st+1;
+            // cout<<i<<" "<<st<<" "<<ans<<" "<<q.front()<<endl;
+            i++;
+        }
+        return ans;
+        
+        
+        
+        
+    }
+};
+
+/*
+
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& a, int k) {
+        
+        int n = a.size();
+        int st =0 , f=0 , s =0 ,i=1,ans=0;
+        unordered_map<int,int> m;
+        m[a[0]]++;
+        while(i<n)
+        {
+            m[a[i]]++;
+            if(a[i]==a[i-1])
+                f = i;
+            else
+            {
+                s= f;
+                f = i;
+            }
+            while(m.size()>2)
+            {
+                m[a[st]]--;
+                if(m[a[st]]==0)
+                    m.erase(a[st]);
+                st++;
+            }
+            if(m.size()==2)
+                ans += (s-st+1);
+            i++;
+        }
+        return ans;
+        
+    }
+};
+
+*/
