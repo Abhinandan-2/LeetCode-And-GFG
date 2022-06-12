44bbbb78e6f43cad26590004503505c622701637
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-12 17:56:14 +0530

    Time: 1086 ms (81.88%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -1,7 +1,6 @@
-# # Write your MySQL query statement below
-# select s.name
-# from salesperson as s 
-# where (s.sales_id not in (select sales_id from orders)) or or (s.sales_id = o.sales_id and o.com_id not in (select com_id from company where name = 'RED'))
-select name
-from salesperson
-where sales_id not in (select sales_id from orders where com_id in (select com_id from company where name = 'RED'))
+select s.name
+from salesperson s
+left join orders o on (o.sales_id = s.sales_id)
+left join company c on (o.com_id = c.com_id)
+group by s.name
+having count(order_id)=0 or not sum(c.name = 'RED')>0
