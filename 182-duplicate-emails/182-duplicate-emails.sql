879aa154a9937f892837641d50b98cb9caee35cb
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-16 00:00:45 +0530

    Time: 388 ms (49.49%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,10 +1,4 @@
-# Write your MySQL query statement below
-select distinct(email) 
-from person
-left join
-(
-select email as emal , count(email) as number
+select email
 from person
 group by email
-) as dup on email = emal
-where number>1
+having count(email)>1
