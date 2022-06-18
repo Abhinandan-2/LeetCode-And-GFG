ebc9da01b5422218d48671e956ab4be84d9684d1
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-18 12:28:59 +0530

    Time: 39 ms (40.77%), Space: 31.2 MB (53.60%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& a) {
+        
+        int n = a.size(),l=0,r=accumulate(a.begin(),a.end(),0);
+        for(int i=0;i<n;i++)
+        {
+            r -= a[i];
+            if(l==r)
+                return i;
+            l+=a[i];
+        }
+        return -1;
+        
+        
+        
+    }
+};
