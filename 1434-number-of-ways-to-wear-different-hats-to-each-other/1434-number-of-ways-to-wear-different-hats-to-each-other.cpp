baaefee8a9dfad31fe80cfbd7d7935fc412ace5f
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-18 21:50:08 +0530

    Time: 49 ms (37.27%), Space: 11.2 MB (19.55%) - LeetHub

--- 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.cpp
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.cpp
@@ -1,42 +1,3 @@
-/*class Solution {
-public:
-    const long MOD = 1e9 + 7;
-    
-    long count(int hat, vector<vector<int>> & p, int people, vector<vector<long>>& dp) {
-        if (people == dp.size()-1) return 1;
-        if (hat > 40) return 0;
-        if (dp[people][hat] != -1) return dp[people][hat];
-        
-        long res = count(hat+1, p, people, dp);
-        
-        for (auto person: p[hat]) if ((people & 1<<person) == 0) {
-            int more = people | 1<<person;
-            res = (res + count(hat+1, p, more, dp)) % MOD;
-        }
-        
-        return dp[people][hat] = res;
-    }
-    
-    int numberWays(vector<vector<int>>& z) {
-        
-        
-        vector<vector<int>> a(41);
-        int n = z.size();
-        for(int i=0;i<z.size();i++)
-        {
-            for(int j=0;j<z[i].size();j++)
-                a[z[i][j]].push_back(i);
-        }
-        
-        vector<vector<long>> dp(1 << n, vector<long>(41, -1));
-        int people = 0;
-        long res = count(1, a, people, dp);
-        return res;
-    }
-};
-
-*/
-
 class Solution {
 public:
     
@@ -81,12 +42,7 @@ public:
             for(int j=0;j<z[i].size();j++)
                 a[z[i][j]].push_back(i);
         }
-        // for(auto i:a)
-        // {
-        //     for(auto j:i)
-        //         cout<<j<<" ";
-        //     cout<<endl;
-        // }
+        
         vector<vector<int>> dp(1<<n,vector<int> (41,-1));
         
         int ans = call(a,1,0,dp);
@@ -96,4 +52,3 @@ public:
         
     }
 };
-
