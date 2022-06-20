900776d0f3a786478521007ee919eb115359c353
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-06-20 13:20:38 +0530

    Time: 123 ms (48.02%), Space: 66.4 MB (44.56%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,32 +1,68 @@
+class node{
+    
+    public :
+    
+    char c;
+    unordered_map<char,node*> m;
+    node(char v)
+    {
+        c = v;
+    }
+    
+};
+
+class trie{
+    
+    node* root;
+    public :
+    
+    trie()
+    {
+        root= new node('1');
+    }
+    
+    bool insert(string &a)
+    {
+        int n = a.size();
+        node *p= root;
+        bool ans = 1;
+        for(int i=0;i<n;i++)
+        {
+            if(p->m.count(a[i]))
+                p=p->m[a[i]];
+            else
+            {
+                ans =0 ;
+                node *t = new node(a[i]);
+                p->m[a[i]] = t;
+                p=p->m[a[i]];
+            }
+        }
+        
+        return ans;
+    }
+    
+};
+
 class Solution {
 public:
     
-    static bool com(string &a,string &b)
-    {
-        int n = a.size() , m = b.size();
-        return m<n;
+    static bool com(string &a,string &b){
+        return a.size()>b.size();
     }
     
     int minimumLengthEncoding(vector<string>& a) {
         
-        unordered_map<string,int> m;
         sort(a.begin(),a.end(),com);
-        int n = a.size(),ans=0,k;
+        int n = a.size(),ans=0;
         string h;
-        
+        trie tree;
         for(int i=0;i<n;i++)
         {
-            k = a[i].size();
-            if(!m.count(a[i]))
-            {
-                ans+= k+1;
-                for(int j=0;j<k;j++)
-                {
-                    h = a[i].substr(j,k-j);
-                    m[h]++;
-                }
-            }
-                
+            h = a[i];
+            reverse(h.begin(),h.end());
+            if(!tree.insert(h))
+                ans += (h.size()+1);
         }
         
         return ans;
