505be84487d4bb56e067ad6ec4f74d44203bd807
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-07-05 20:27:02 +0530

    Time: 7 ms (70.75%), Space: 8 MB (47.34%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,23 +4,16 @@ public:
         
         if(n==0)
             return  {0};
-        if(n==1)
-            return {0,1};
-        if(n==2)
-            return {0,1,1};
+       
         vector<int> ans(n+1);
         ans[0] = 0;
-        ans[1] = 1;
-        ans[2] = 1;
-        int sub = 2;
+        int sub = 1;
         
-        for(int i=3;i<=n;i++)
+        for(int i=1;i<=n;i++)
         {
             if(sub*2==i)
                 sub = i;
-            // co/ut<<i<<" "<<sub<<" ";
             ans[i] = ans[i-sub]+1;
-            // cout<<sub<<" ";
         }
         
         return ans;
