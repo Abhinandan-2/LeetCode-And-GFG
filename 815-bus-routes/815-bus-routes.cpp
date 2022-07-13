a4d2cb833d37187fa2bbd2ddae3fba2779be6a67
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-07-13 13:45:18 +0530

    Time: 365 ms (57.15%), Space: 62.5 MB (67.49%) - LeetHub

--- 815-bus-routes/815-bus-routes.cpp
+++ 815-bus-routes/815-bus-routes.cpp
@@ -2,73 +2,70 @@ class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
         
-        if(source==target)
+        if(source==target)   // If source is target satation no need to use bus 
             return 0;
-        unordered_map<int,vector<int>> stations;
-        unordered_map<int,int> bus,station;
+        unordered_map<int,vector<int>> stations;  /* store which buses travels that station
+
+    Like for - routes = [[1,2,7],[3,6,7]]
+
+    stations[1] = [0]
+    stations[2] = [0]
+    stations[7] = [0,1]
+    stations[3] = [1]
+    stations[6] = [1]
+    
+    */
+    
+        unordered_map<int,int> visitedBus,visitedStation;  // store which buses and stations are visited
         
-        for(int i=0;i<routes.size();i++)
-        {
+        for(int i=0;i<routes.size();i++)        // Initializing the stations unordered_map as show above
             for(int j=0;j<routes[i].size();j++)
-            {
                 stations[routes[i][j]].push_back(i);
-            }
-        }
         
-        // for(auto i:stations)
-        // {
-        //     cout<<i.first<<" ";
-        //     for(auto j:i.second)
-        //     {
-        //         cout<<j<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         
-        int ans =0 ,h,p;
+        // Now it's time to BFS
+        
+        int ans =0 ,size, curBus ;
         queue<int> q;
-        if(!station.count(source)){
-        station[source]++;
-        for(int i=0;i<stations[source].size();i++)
+       
+        for(int i=0;i<stations[source].size();i++)   // store all buses in queue that travels source station
         {
-            if(!bus.count(stations[source][i])){
             q.push(stations[source][i]);
-            bus[stations[source][i]]++;
-            }
+            visitedBus[stations[source][i]]++;   // mark bus as visited
         }
-        }
+        visitedStation[source]++;  // mark station as visited
+        
         
         while(!q.empty())
         {
-            h = q.size();
+            size = q.size();   
             ans++;
-            while(h--)
+            while(size--) // visit all station those will travels by all buses that are currently in queue (like we do in BFS)
             {
-                p = q.front();
+                curBus = q.front();
                 q.pop();
-                // cout<<p<<" ";
-                for(int i=0;i<routes[p].size();i++)
+                for(int i=0;i<routes[curBus].size();i++)
                 {
-                    if(routes[p][i]==target)
+                    if(routes[curBus][i]==target)  // If station is target station return the ans
                         return ans;
-                    int &st = routes[p][i];
-                    if(!station.count(st))
+                    int &st = routes[curBus][i];  // Otherwise visit that station and store all buses that will travels that (st) station
+                    if(!visitedStation.count(st))
                     {
-                        station[st]++;
                         for(int j=0;j<stations[st].size();j++)
                         {
-                            if(!bus.count(stations[st][j]))
+                            if(!visitedBus.count(stations[st][j]))
                             {
                                 q.push(stations[st][j]);
-                                bus[stations[st][j]]++;
+                                visitedBus[stations[st][j]]++;  // mark se visited
                             }
                         }
+                        visitedStation[st]++;  // mark as visited
                     }
                 }
             }
         }
         
-        return -1;
+        return -1;   // If target not found return -1
         
         
     }
