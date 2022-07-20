1ef5977c4398dc75f060c17622a028e5c6badabb
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-07-21 01:42:04 +0530

    Time: 129 ms (43.23%), Space: 28.3 MB (35.46%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& a, int k) {
+        
+        
+        int n = a.size();
+        int h,p;
+        pair<int,int> q;
+        if(n%k!=0)
+            return 0;
+        
+        int t = n/k;
+        
+        map<int,int> m;
+        
+        for(auto i:a)
+            m[i]++;
+        
+        while(t--)
+        {
+            q = *m.begin();
+            p = q.first;
+            h = k;
+            while(m.count(p)&&h)
+            {
+                m[p]--;
+                if(m[p]==0)
+                    m.erase(p);
+                p++;
+                h--;
+            }
+            if(h!=0)
+                return 0;
+        }
+        
+        return 1;
+        
+        
+    }
+};
