c96483db9f68ff504e388609aae82ad400f4fa36
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-07-29 14:53:49 +0530

    Time: 151 ms (48.29%), Space: 28.3 MB (23.40%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,5 +1,3 @@
-
-
 class Solution {
 public:
     
@@ -9,13 +7,13 @@ public:
         int x[] = {0,0,1,-1};
         int y[] = {1,-1,0,0};
         
-        vector<vector<int>> dp(n,vector<int> (m,INT_MAX));
+        vector<vector<int>> dis(n,vector<int> (m,INT_MAX));
         vector<vector<bool>> v(n,vector<bool> (m,false));
         
         list<vector<int>> q;
         vector<int> p;
         int x1,y1,cost;
-        dp[0][0]=0 ;
+        dis[0][0]=0 ;
         q.push_front({0,0,0});
         
         while(!q.empty())
@@ -29,19 +27,19 @@ public:
                 x1 = p[0]+x[k];
                 y1 = p[1]+y[k];
                 cost = a[p[0]][p[1]] == k+1 ? 0 :1 ;
-                if(x1>=0&&x1<n&&y1>=0&&y1<m&&v[x1][y1]==false&&p[2]+cost<dp[x1][y1])
+                if(x1>=0&&x1<n&&y1>=0&&y1<m&&v[x1][y1]==false&&p[2]+cost<dis[x1][y1])
                 {
-                    dp[x1][y1] = p[2]+cost;
+                    dis[x1][y1] = p[2]+cost;
                     if(cost==0)
-                        q.push_front({x1,y1,dp[x1][y1]});
+                        q.push_front({x1,y1,dis[x1][y1]});
                     else
-                        q.push_back({x1,y1,dp[x1][y1]});
+                        q.push_back({x1,y1,dis[x1][y1]});
                 }
             }
             
         }
         
-        return dp[n-1][m-1];
+        return dis[n-1][m-1];
          
     }
 };
