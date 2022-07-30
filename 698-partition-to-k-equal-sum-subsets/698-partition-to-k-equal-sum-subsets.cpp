793363b4025fcd66aabfd002bb045e5bf37d849b
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-07-30 19:56:38 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (86.30%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,61 @@
+class Solution {
+    
+    bool isEqual(vector<int> &a,int t)
+    {
+        for(auto i:a)
+        {
+            if(i!=t)
+                return 0;
+        }
+        return 1;
+    }
+    
+    bool fillLen(vector<int> &a,int i,int t,vector<int> &len)
+    {
+        // cout<<i<<endl;
+        // for(auto i:len)
+        //     cout<<i<<" ";
+        // cout<<endl;
+        int n = a.size();
+        if(isEqual(len,t))
+            return 1;
+        int m = len.size(),k;
+        for(int j=0;j<m;j++)
+        {
+            k = j-1;
+            while(k>=0)
+            {
+                if(len[j]==len[k])
+                    break;
+                k--;
+            }
+            if(k!=-1)
+                continue;
+            if(len[j]+a[i]<=t)
+            {
+                len[j] += a[i];
+                if(fillLen(a,i+1,t,len))
+                    return 1;
+                len[j] -= a[i];
+            }
+        }
+        return 0;
+    }
+    
+public:
+    bool canPartitionKSubsets(vector<int>& a, int k) {
+        
+        int sum = accumulate(a.begin(),a.end(),0);
+        if(sum%k!=0)
+            return 0;
+        int t = sum/k;
+        sort(a.begin(),a.end(),greater<int>());
+        if(a[0]>t)
+            return 0;
+        
+        vector<int> len(k,0);
+        // cout<<t<<" ";
+        return fillLen(a,0,t,len);
+        
+    }
+};
