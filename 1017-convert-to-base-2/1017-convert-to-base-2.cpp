91e8d77377b5ee9f34b117aa98410d2ea438f6d0
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-08-13 01:32:53 +0530

    Time: 5 ms (19.57%), Space: 6.1 MB (10.14%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        
+        if(n==0)
+            return "0";
+        string ans;
+        int base = -2, remainder;
+        while(n!=0)
+        {
+            remainder = n%base;
+            n /= -2;
+            if(remainder==-1)
+            {
+                remainder += (-base) ;
+                n++;
+            }
+            ans.push_back(remainder+48);
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
