dc6aa3c1e585a8b5a05e8bc6473a32f34f9885db
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-08-13 23:39:54 +0530

    Time: 317 ms (53.18%), Space: 91.2 MB (64.47%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -4,46 +4,46 @@ class Solution {
 public:
     long long maxAlternatingSum(vector<int>& a) {
         
-        ll n= a.size() , f  =0, ans =0,l= a[0] ;
+        ll n= a.size() , flag  =0, ans =0,last = a[0] ;
         ans = a[0];
         for(int i=1;i<n;i++)
         {
             if(a[i]==a[i-1])
                 continue;
-            if(f==0)
+            if(flag==0)
             {
-                if(a[i]>a[i-1])
+                if(a[i]>last)
                 {
-                    ans-=l;
+                    ans-=last;
                     ans+= a[i];
-                    l = a[i];
+                    last = a[i];
                 }
                 else
                 {
                     ans -= a[i];
-                    l =a[i];
-                    f=1;
+                    last=a[i];
+                    flag=1;
                 } 
             }
             else
             {
-                if(a[i]<a[i-1])
+                if(a[i]<last)
                 {
-                    ans += l;
+                    ans +=last;
                     ans -= a[i];
-                    l = a[i];
+                    last = a[i];
                 }
                 else
                 {
                     ans += a[i];
-                    l = a[i];
-                    f=0;
+                    last = a[i];
+                    flag=0;
                 }
             }
         }
         
-        if(f==1)
-            ans += l;
+        if(flag==1)
+            ans += last;
         return ans;
         
         
