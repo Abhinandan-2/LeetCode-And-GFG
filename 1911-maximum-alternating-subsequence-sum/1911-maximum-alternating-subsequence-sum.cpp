0d787dae8c499f0298805c068487bb5734fbdfb5
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-08-13 23:46:54 +0530

    Time: 285 ms (63.22%), Space: 91.2 MB (72.52%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -4,45 +4,49 @@ class Solution {
 public:
     long long maxAlternatingSum(vector<int>& a) {
         
-        ll n= a.size() , flag  =0, ans =0,last = a[0] ;
+        ll n= a.size() , flag  =0, ans =0,last ; 
+		// flag = 0 -> In search of local max
+		// flag = 1 -> In search of local min
+		// last -> What is the last value added or subtracted in ans
         ans = a[0];
+		last = a[0];
         for(int i=1;i<n;i++)
         {
-            if(a[i]==a[i-1])
+            if(a[i]==a[i-1])       // If same value no need to do anything beacuse it will not help in finding local max or local min
                 continue;
-            if(flag==0)
+            if(flag==0)             // In search of local max
             {
-                if(a[i]>last)
+                if(a[i]>last)          // If cur value greater than last value it means last is not the local max and cur shoud be local max
                 {
-                    ans-=last;
+                    ans-=last;          // Sub last and Add Cur value
                     ans+= a[i];
                     last = a[i];
                 }
-                else
+                else                       // It means last is local max and cur should be local min
                 {
-                    ans -= a[i];
+                    ans -= a[i];        // Local min -> Sub from ans
                     last=a[i];
-                    flag=1;
+                    flag=1;            // Turn flag 1 beacuase now we will in search of local min
                 } 
             }
-            else
+            else                   // In search of local min
             {
-                if(a[i]<last)
+                if(a[i]<last)           //  If cur value less than last value it means last is not the local min and cur shoud be local min
                 {
-                    ans +=last;
+                    ans +=last;    // Add last and Sub Cur value
                     ans -= a[i];
                     last = a[i];
                 }
-                else
+                else              // It means last is local min and cur should be local max
                 {
-                    ans += a[i];
+                    ans += a[i];   // Local max -> Add in ans
                     last = a[i];
-                    flag=0;
+                    flag=0;          // Turn flag 0 beacuase now we will in search of local max
                 }
             }
         }
         
-        if(flag==1)
+        if(flag==1)            // If last value is for local min, no need to Sub from ans. So, Add it in ans to maxmize the answer
             ans += last;
         return ans;
         
