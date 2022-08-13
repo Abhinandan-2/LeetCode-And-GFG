901ae81df51553309ccfede8e1cfc695ce0eb4fe
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-08-14 00:27:23 +0530

    Time: 212 ms (88.92%), Space: 91.2 MB (72.52%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -4,39 +4,48 @@ class Solution {
 public:
     long long maxAlternatingSum(vector<int>& a) {
         
-        ll n= a.size() , flag  =0, ans =0, val = a[0], cur ;
-        // flag = 0 -> In search of local max
-		// flag = 1 -> In search of local min
+        ll n= a.size() , f  =0, ans =0,l= a[0] ;
+        ans = a[0];
         for(int i=1;i<n;i++)
         {
-            if(a[i]==a[i-1])    // If the values are the same, there is no need to do anything because it will not help in determining the local maximum or minimum.
+            if(a[i]==a[i-1])
                 continue;
-            if(flag==0)        // In search of local max
+            if(f==0)
             {
-                if(a[i]>val)     // Maximise the val
-                   val = a[i];
+                if(a[i]>a[i-1])
+                {
+                    ans-=l;
+                    ans+= a[i];
+                    l = a[i];
+                }
                 else
                 {
-                    ans += val;  // Add local max to ans
-                    val = a[i];  
-                    flag = 1;    // Turn flag 1 because now we will be in search of local min.
+                    ans -= a[i];
+                    l =a[i];
+                    f=1;
                 } 
             }
-            else                // In search of local min
+            else
             {
-                if(a[i]<val)    // Minimise the val
-                    val = a[i];
+                if(a[i]<a[i-1])
+                {
+                    ans += l;
+                    ans -= a[i];
+                    l = a[i];
+                }
                 else
                 {
-                    ans -= val; // Sub from ans
-                    val = a[i];
-                    flag = 0;   // Turn flag 0 because we will now be looking for local max.
+                    ans += a[i];
+                    l = a[i];
+                    f=0;
                 }
             }
         }
         
-        if(flag==0)           // If last search for local max then add it to ans
-            ans += val;
+        if(f==1)
+            ans += l;
         return ans;
+        
+        
     }
 };
