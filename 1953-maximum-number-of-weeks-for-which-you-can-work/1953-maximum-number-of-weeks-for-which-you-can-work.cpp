55412487813d0d5ec96c7db3051201ef2e0139ed
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-08-23 01:12:10 +0530

    Time: 191 ms (71.38%), Space: 75.5 MB (91.26%) - LeetHub

--- 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.cpp
+++ 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.cpp
@@ -2,65 +2,26 @@ class Solution {
 public:
     long long numberOfWeeks(vector<int>& a) {
         
-        int n = a.size(),p;
-        long long sum=0,h;
-        sort(a.begin(),a.end());
-        for(auto i:a)
-            sum+=i;
-        
+        int n = a.size();
         if(n==1)
             return 1;
-        // if(n==2)
-        // {
-        //     p = abs(a[0]-a[1]);
-        //     if(p)
-        //     p--;
-        //     return sum-p;
-        // }
-//         if(n==3)
-//         {
-            
-//             p = a[2]-a[1];
-//             p = abs(p-a[0]);
-//             if(p)
-//                 p--;
-//             return sum-p;
-//         }
         
-        h = sum/2;
-        // cout<<h;
-        if(a[n-1]>h)
+        long long sum=0,h,mx=0;
+        
+        for(auto i:a)
         {
-            h = sum-a[n-1];
+            sum+=i;
+            mx = max(mx,(long long)i);
+        }
+        h = sum/2;
+        if(mx>h)
+        {
+            h = sum-mx;
             h *=2;
             h++;
             return h;
         }
         return sum;
 
-//         priority_queue<int> q;
-        
-//         for(int i=0;i<n;i++)
-//             q.push(a[i]);
-        
-//         while(q.size()>1)
-//         {
-//             p = q.top();
-//             q.pop();
-//             r = q.top();
-//             q.pop();
-//             p-=r;
-//             if(p!=0)
-//             q.push(p);
-//         }
-        
-//         int rem;
-//         if(q.empty())
-//             rem=0;
-//         else
-//             rem = q.top()-1;
-        
-//         return sum-rem;
-        
     }
 };
