565aa860085b98073e368cd50f009ca718f70b27
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-07 21:43:39 +0530

    Time: 86 ms (19.89%), Space: 18.8 MB (28.20%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -1,60 +1,39 @@
+#define ll long long
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(vector<int>& a, int k, int t) {
-        
-        if(k==0)
-            return 0;
-        multiset<int> s;
-        int n = a.size(),h;
-        for(int i=0;i<min(k+1,n);i++)
-        {
-            s.insert(a[i]);
-        }
-        long long ll;
+    bool containsNearbyAlmostDuplicate(vector<int>& a, int k, int t) { 
+        if(k==0) return 0;
+        int n = a.size();
+        ll dif;
         multiset<int>::iterator it;
-        for(auto it=s.begin();it!=s.end();it++)
-        {
-            auto ij = it;
-            ij++;
+        multiset<int> s;
+        for(int i=0;i<min(k+1,n);i++) s.insert(a[i]);
+        for(auto it=s.begin();it!=s.end();it++){
+            auto ij = it;ij++;
             if(ij!=s.end()){
-            ll = abs((long long)(*it)-(long long)(*ij));
-            if(ll<=(long long)t)
-                return 1;
+                dif = abs((ll)(*it)-(ll)(*ij));
+                if(dif<=(ll)t) return 1;
             }
         }
-        
-        for(int i=k+1;i<n;i++)
-        {
-            h = a[i-k-1];
-            auto it = s.find(h);
-            s.erase(it);
+        for(int i=k+1;i<n;i++){
+            s.erase(s.find(a[i-k-1]));
             auto ij = s.lower_bound(a[i]);
-            if(ij==s.end())
-            {
+            if(ij==s.end()){
                 ij--;
-                h = *ij;
-                ll = abs((long long)(h)-(long long)(a[i]));
-                if(ll<=(long long)t)
-                    return 1;
+                dif = abs((ll)(*ij)-(ll)(a[i]));
+                if(dif<=(ll)t) return 1;
                 s.insert(a[i]);
                 continue;
             }
-            h = *ij;
-            ll = abs((long long)(h)-(long long)(a[i]));
-            if(ll<=(long long)t)
-                return 1;
-            if(ij!=s.begin())
-            {
+            dif = abs((ll)(*ij)-(ll)(a[i]));
+            if(dif<=(ll)t) return 1;
+            if(ij!=s.begin()){
                 ij--;
-                h = *ij;
-                ll = abs((long long)(h)-(long long)(a[i]));
-                if(ll<=(long long)t)
-                    return 1;
+                dif = abs((ll)(*ij)-(ll)(a[i]));
+                if(dif<=(ll)t) return 1;
             }
             s.insert(a[i]);  
         }
-        
         return 0;
-        
     }
 };
