d4325d47053367b51b00161fd5c41c56156dea6d
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-08 19:06:00 +0530

    Time: 1551 ms (69.66%), Space: 47.9 MB (23.80%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,61 +1,30 @@
 class Solution {
 public:
     int minimizeTheDifference(vector<vector<int>>& a, int tar) {
-        
-        int n = a.size() , m =a[0].size() , dif = INT_MAX ,help,num,ans = INT_MAX;
+        int n = a.size(),m =a[0].size(),dif=INT_MAX,help,num,ans=INT_MAX;
         unordered_set<int> s,p;
-        for(int j=0;j<m;j++)
-        {
-            if(a[0][j]>tar)
-            {
+        for(int j=0;j<m;j++){
+            if(a[0][j]>tar){
                 help = abs(tar-a[0][j]);
-                if(help<dif)
-                {
-                    s.insert(a[0][j]);
-                    dif = help;
-                }
+                if(help<dif){s.insert(a[0][j]);dif = help;}
             }
-            else
-            {
-                s.insert(a[0][j]);
-            }
+            else s.insert(a[0][j]);
         }
-        // for( auto i:s)
-        //     cout<<i<<" ";
-        // cout<<endl;
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            { 
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){ 
                 dif = INT_MAX;
-                for(auto k:s)
-                {
+                for(auto k:s){
                     num = a[i][j]+k;
-                    if(num>tar)
-                    {
+                    if(num>tar){
                         help = abs(tar-num);
-                        if(help<dif)
-                        {
-                            dif = help;
-                            p.insert(num);
-                        }
+                        if(help<dif){dif = help;p.insert(num);}
                     }
-                    else
-                        p.insert(num);
-                }
-                
+                    else p.insert(num);
+                } 
             }
-            s = p;
-            p.clear();
+            s = p; p.clear();
         }
-        
-        for(auto k:s)
-        {
-            ans = min(ans,abs(tar-k));
-        }
-        
+        for(auto k:s) ans = min(ans,abs(tar-k));
         return ans;
-         
-        
     }
 };
