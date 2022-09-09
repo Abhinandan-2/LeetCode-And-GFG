72f1d6de24f0f16e4b8b9a52c19c947e19a84f40
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-10 01:38:54 +0530

    Time: 352 ms (12.52%), Space: 15.1 MB (27.03%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     int sumFourDivisors(vector<int>& a) {
-        
-        int ans= 0,sum =0, count,num ,n = a.size();
+        int ans= 0,sum =0, count,n = a.size();
         unordered_map<int,int> m;
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             if(m.count(a[i])){ans += m[a[i]];continue;}
-            num = a[i];
-            count =0;
-            sum =0;
-            for(int j=1;j<=sqrt(num);j++)
-            {
-                if(num%j==0)
-                {
-                    sum += j;
-                    count++;
-                    if(num/j!=j)
-                    {
-                        sum += num/j;
-                        count++;
-                    }
+            count =0;sum =0;
+            for(int j=1;j<=sqrt(a[i]);j++){
+                if(a[i]%j==0){
+                    sum += j;count++;
+                    if(a[i]/j!=j){sum += a[i]/j;count++;}
                 }
             }
-            if(count==4) m[num]=sum;
+            if(count==4) m[a[i]]=sum;
             else m[a[i]] =0;
             ans += m[a[i]];
         }
-        
         return ans;
-        
     }
 };
