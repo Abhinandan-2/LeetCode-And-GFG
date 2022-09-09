0e4dc2008c0ac5b456fb3853b5d8d17e3620489d
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-10 01:26:54 +0530

    Time: 271 ms (7.87%), Space: 44 MB (41.84%) - LeetHub

--- 1054-distant-barcodes/1054-distant-barcodes.cpp
+++ 1054-distant-barcodes/1054-distant-barcodes.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     vector<int> rearrangeBarcodes(vector<int>& a) {
-        
         int n = a.size();
         unordered_map<int,int> m;
-        for(auto i:a)
-            m[i]++;
+        for(auto i:a) m[i]++;
         priority_queue<pair<int,int>> q;
         pair<int,int> p,h;
-        for(auto i:m)
-        {
-            q.push({i.second,i.first});
-        }
+        for(auto i:m) q.push({i.second,i.first});
         vector<int> ans;
-        p = q.top();
-        q.pop();
+        p = q.top();q.pop();
         ans.push_back(p.second);
-        while(!q.empty())
-        {
-            h = q.top();
-            q.pop();
+        while(!q.empty()){
+            h = q.top();q.pop();
             ans.push_back(h.second);
-            if(p.first!=1)
-            q.push({p.first-1,p.second});
+            if(p.first!=1) q.push({p.first-1,p.second});
             p = h;
         }
         return ans;
-        
     }
 };
