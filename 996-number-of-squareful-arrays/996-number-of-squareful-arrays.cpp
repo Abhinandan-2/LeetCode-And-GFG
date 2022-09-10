387b96e6b4992efae9b8913c02ac3378bb7d0b8d
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-10 23:59:54 +0530

    Time: 8 ms (32.70%), Space: 8.2 MB (30.59%) - LeetHub

--- 996-number-of-squareful-arrays/996-number-of-squareful-arrays.cpp
+++ 996-number-of-squareful-arrays/996-number-of-squareful-arrays.cpp
@@ -1,54 +1,36 @@
 class Solution {
-    
-    bool isAllvisited(vector<int> &v)
-    {
-        for(auto i:v)
-            if(i==-1)
-                return 0;
+    bool isAllvisited(vector<int> &v){
+        for(auto i:v) if(i==-1) return 0;
         return 1;
     }
-    
-    void call(vector<int> &a,vector<int> &v,int &ans,int last)
-    {
+    void call(vector<int> &a,vector<int> &v,int &ans,int last){
         int n = a.size(),pow,sq;
-        if(isAllvisited(v))
-        {
-            ans++;
-            return ;
-        }
+        if(isAllvisited(v)){ans++;return ;}
         unordered_map<int,int> m;
-        for(int i=0;i<n;i++)
-        {
-            if(v[i]==-1&&!m.count(a[i]))
-            {
-                pow = last+a[i];
-                sq = sqrt(pow);
-                if(pow==sq*sq)
-                {
+        for(int i=0;i<n;i++){
+            if(v[i]==-1&&!m.count(a[i])){
+                sq = sqrt(last+a[i]);
+                if(last+a[i]==sq*sq){
                     v[i]=1;
                     call(a,v,ans,a[i]);
                     v[i]=-1;
                 }
                 m[a[i]]++;
             }
-            
         }
         return ;
-    }
-    
+    }   
 public:
     int numSquarefulPerms(vector<int>& a) {
-        
         int n = a.size() ,ans=0;
         vector<int> v(n,-1);
         unordered_map<int,int> m;
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             if(!m.count(a[i])){
-            v[i] = 1;
-            call(a,v,ans,a[i]);
-            v[i] = -1;
-            m[a[i]]++;
+                v[i] = 1;
+                call(a,v,ans,a[i]);
+                v[i] = -1;
+                m[a[i]]++;
             }
         }
         return ans;
