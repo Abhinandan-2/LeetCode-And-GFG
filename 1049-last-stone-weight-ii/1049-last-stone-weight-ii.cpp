7c3ec0984fc159593d05611ffe0ab1ac9df33280
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-10 14:35:23 +0530

    Time: 9 ms (58.73%), Space: 8.4 MB (63.24%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& a) {
-        
-        int n = a.size();
+        int n = a.size(),sum = accumulate(a.begin(),a.end(),0);
         if(n==1) return a[0];
-        if(n==2) return abs(a[1]-a[0]);
-        int sum = accumulate(a.begin(),a.end(),0);
         int tar = sum/2,ans=0;
         vector<int> v(tar+1,0) , h(tar+1,0);
         h[0] = 1;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<=tar;j++)
-            {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=tar;j++){
                 if(h[j]==1) {v[j] = 1;continue;}
-                if(j<a[i]) v[j] = 0;
-                else
-                {
-                    if(h[j-a[i]]==1) v[j]= 1;
-                    else v[j] = 0;
-                }
+                j<a[i] ? v[j] = 0 : h[j-a[i]]==1 ? v[j]= 1 :v[j] = 0;
                 if(v[j]==1) ans = max(ans,j);
             }
             if(ans==tar) break;
             h =v;
         }
-        
-        int res = sum-ans;
-        return abs(ans-res);
-        
+        return abs(ans-(sum-ans));
     }
 };
