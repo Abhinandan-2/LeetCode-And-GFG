4826645e0ebe97312c3c3090a48de982abe2a9db
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-10 16:04:45 +0530

    Time: 77 ms (83.94%), Space: 14.6 MB (90.08%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,52 +1,33 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& a) {
-        
         int n = a.size() ;
         if(n==1) return a[0][0];
-        
-        int fm = INT_MAX, pos ,sm=INT_MAX, mn,in,mn2,num;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(a[0][i]<=fm)
-            {
+        int fm = INT_MAX, pos ,sm=INT_MAX, mn,in,mn2;
+        for(int i=0;i<n;i++){
+            if(a[0][i]<=fm){
                 pos = i;
                 sm = fm;
                 fm = a[0][i];
             }
-            else if(a[0][i]<sm)
-                sm = a[0][i];     
+            else if(a[0][i]<sm) sm = a[0][i];     
         }
-        for(int i=1;i<n;i++)
-        {
-            mn = INT_MAX;
-            mn2 = INT_MAX;
-            for(int j=0;j<n;j++)
-            {
-                num = a[i][j];
-                if(pos==j)
-                {
-                    num += sm;
-                }
-                else
-                {
-                    num += fm;
-                }
-                if(num<=mn)
-                {
+        for(int i=1;i<n;i++){
+            mn = INT_MAX;mn2 = INT_MAX;
+            for(int j=0;j<n;j++){
+                if(pos==j) a[i][j] += sm;
+                else a[i][j] += fm;
+                if(a[i][j]<=mn){
                     in = j;
                     mn2 = mn;
-                    mn = num;
+                    mn = a[i][j];
                 }
-                else if(num<mn2)
-                    mn2 = num;
+                else if(a[i][j]<mn2) mn2 = a[i][j];
             }
             pos = in;
             fm = mn;
             sm = mn2;
         }
         return fm;
-        
     }
 };
