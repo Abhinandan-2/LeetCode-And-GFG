49f121b5e9cf46489a6eb7ccca3013d1c7cc716e
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-10 12:42:45 +0530

    Time: 4 ms (31.23%), Space: 6.4 MB (25.68%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -1,61 +1,24 @@
 class Solution {
-    
-    bool isValid(string &a,string &b,int x,int y)
-    {
-        int n = a.size();
+    bool isValid(string &a,string &b,int x,int y,int n){
         int i = (x+1)%n, j =(y+1)%n;
-        while(i!=x)
-        {
-            if(a[i]!=b[j])
-                return 0;
-            i++;
-            j++;
-            i %= n;
-            j %= n;
+        while(i!=x){
+            if(a[i++]!=b[j++]) return 0;
+            i %= n; j %= n;
         }
-        
-        return 1;
-        
+        return 1; 
     }
-    
 public:
     bool rotateString(string a, string b) {
-        
-        int n = a.size() , ms = b.size();
+        int n = a.size() , ms = b.size(),mn = INT_MAX,pos;
         if(n!=ms) return 0;
         unordered_map<char,int> m,v;
         for(auto i:a) m[i]++;
         for(auto i:b) v[i]++;
         if(m!=v) return 0;
-        int mn = INT_MAX,pos;
         char c;
-        for(auto i:m)
-        {
-            if(mn>i.second)
-            {
-                mn = i.second;
-                c = i.first;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(a[i]==c)
-            {
-                pos = i;
-                break;
-            }
-            
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            if(b[i]==c)
-            {
-                if(isValid(a,b,pos,i))
-                    return 1;
-            }
-        }
+        for(auto i:m) if(mn>i.second){mn = i.second;c = i.first;}
+        for(int i=0;i<n;i++) if(a[i]==c){ pos = i;break;}
+        for(int i=0;i<n;i++) if(b[i]==c&&isValid(a,b,pos,i,n)) return 1;
         return 0;
-        
     }
 };
