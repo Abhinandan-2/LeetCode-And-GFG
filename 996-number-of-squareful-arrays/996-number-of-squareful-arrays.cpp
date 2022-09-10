a4d3892df9c9a5cb0a13514b4d801bde053484e1
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-10 23:57:50 +0530

    Time: 5 ms (49.33%), Space: 8.3 MB (30.59%) - LeetHub

---
+++ 996-number-of-squareful-arrays/996-number-of-squareful-arrays.cpp
@@ -0,0 +1,56 @@
+class Solution {
+    
+    bool isAllvisited(vector<int> &v)
+    {
+        for(auto i:v)
+            if(i==-1)
+                return 0;
+        return 1;
+    }
+    
+    void call(vector<int> &a,vector<int> &v,int &ans,int last)
+    {
+        int n = a.size(),pow,sq;
+        if(isAllvisited(v))
+        {
+            ans++;
+            return ;
+        }
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++)
+        {
+            if(v[i]==-1&&!m.count(a[i]))
+            {
+                pow = last+a[i];
+                sq = sqrt(pow);
+                if(pow==sq*sq)
+                {
+                    v[i]=1;
+                    call(a,v,ans,a[i]);
+                    v[i]=-1;
+                }
+                m[a[i]]++;
+            }
+            
+        }
+        return ;
+    }
+    
+public:
+    int numSquarefulPerms(vector<int>& a) {
+        
+        int n = a.size() ,ans=0;
+        vector<int> v(n,-1);
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++)
+        {
+            if(!m.count(a[i])){
+            v[i] = 1;
+            call(a,v,ans,a[i]);
+            v[i] = -1;
+            m[a[i]]++;
+            }
+        }
+        return ans;
+    }
+};
