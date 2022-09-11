b84fc6db714c03dbbb70c9e29942671d41da6d2a
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-11 16:03:54 +0530

    Time: 165 ms (20.85%), Space: 40.3 MB (87.82%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -3,48 +3,20 @@ public:
     int maxTurbulenceSize(vector<int>& a) {
         int n = a.size(),f=0,ans =0 ,c=1;
         if(n==1) return 1;
-        for(int i=1;i<n;i++)
-        {
-            if(a[i]==a[i-1])
-            {
-                ans = max(ans,c);
-                c=1;
-                f=0;
-            }
-            else
-            {
-                if(a[i]>a[i-1])
-                {
-                    if(f==1||f==0)
-                    {
-                        c++;
-                        f=-1;
-                    }
-                    else
-                    {
-                        ans = max(ans,c);
-                        c=2;
-                        f=-1;
-                    }
+        for(int i=1;i<n;i++){
+            if(a[i]==a[i-1]){ans = max(ans,c);c=1;f=0;}
+            else{
+                if(a[i]>a[i-1]){
+                    if(f==1||f==0){c++;f=-1;}
+                    else{ans = max(ans,c);c=2;  }
                 }
-                else
-                {
-                    if(f==-1||f==0)
-                    {
-                        c++;
-                        f=1;
-                    }
-                    else
-                    {
-                        ans= max(ans,c);
-                        c=2;
-                        f=1;
-                    }
+                else{
+                    if(f==-1||f==0){c++;f=1;}
+                    else{ans= max(ans,c);c=2;}
                 }
             }
         }
         ans = max(ans,c);
         return ans;
-        
     }
 };
