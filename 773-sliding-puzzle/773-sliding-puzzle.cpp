f916d625d3f9162d419cc519f78c49727306a67c
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-19 01:08:21 +0530

    Time: 64 ms (20.46%), Space: 11.1 MB (28.90%) - LeetHub

--- 773-sliding-puzzle/773-sliding-puzzle.cpp
+++ 773-sliding-puzzle/773-sliding-puzzle.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int slidingPuzzle(vector<vector<int>>& a) {
-        
         map<vector<vector<int>>,int> m;
         queue<vector<vector<int>>> q;
         vector<vector<int>> p,h,res ={{1,2,3},{4,5,0}};
@@ -9,49 +8,30 @@ public:
         int ans = -1,x2,y2,x1,y1,sz;
         int x[] = {0,0,1,-1} , y[] = {1,-1,0,0};
         m[a]++;
-        while(!q.empty())
-        {
+        while(!q.empty()){
             ans++;
             sz = q.size();
-            while(sz--)
-            {
-                p = q.front();
-                q.pop();
-                if(p==res)
-                    return ans;
+            while(sz--){
+                p = q.front(); q.pop();
+                if(p==res) return ans;
                 for(int i=0;i<2;i++)
-                {
                     for(int j=0;j<3;j++)
-                    {
-                        if(p[i][j]==0)
-                        {
-                            x1= i;
-                            y1 =j;
-                        }
-                    }
-                }
-                
-                for(int k=0;k<4;k++)
-                {
+                        if(p[i][j]==0){x1= i;y1 =j;}
+                for(int k=0;k<4;k++){
                     x2 = x1+x[k];
                     y2 = y1+y[k];
-                    if(x2>=0&&x2<2&&y2>=0&&y2<3)
-                    {
+                    if(x2>=0&&x2<2&&y2>=0&&y2<3){
                         h = p;
                         h[x2][y2] = p[x1][y1];
                         h[x1][y1] = p[x2][y2];
-                        if(!m.count(h))
-                        {
+                        if(!m.count(h)){
                             q.push(h);
                             m[h]++;
                         }
                     }
                 }
-                
             }
         }
-        
-        return -1;
-        
+        return -1;   
     }
 };
