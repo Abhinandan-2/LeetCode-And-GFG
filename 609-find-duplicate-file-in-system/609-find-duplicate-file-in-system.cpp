e8116ebcada613ca0673499b69e4dbf74526d4aa
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-09-19 15:39:14 +0530

    Time: 204 ms (40.46%), Space: 39.3 MB (58.01%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     vector<vector<string>> findDuplicate(vector<string>& a) {
-        
         int n = a.size(),len,f;
         vector<vector<string>> ans;
-        // vector<string> res;
         string help,path,content,file;
         unordered_map<string,vector<string>> mp;
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             stringstream c(a[i]);
             f=0;
-            while(getline(c,help,' '))
-            {
-                if(f==0)
-                {
-                    path = help;
-                    f++;
-                }
-                else
-                {
-                    len = help.size();
-                    len-=2;
-                    while(help[len]!='(')
-                        content.push_back(help[len--]);
+            while(getline(c,help,' ')){
+                if(f++==0) path = help;
+                else{
+                    len = help.size()-2;
+                    while(help[len]!='(') content.push_back(help[len--]);
                     len--;
-                    while(len>=0)
-                        file.push_back(help[len--]);
+                    while(len>=0) file.push_back(help[len--]);
                     reverse(file.begin(),file.end());
                     mp[content].push_back(path+"/"+file);
                     content.clear();
@@ -34,12 +22,7 @@ public:
                 }
             }
         }
-        
-        for(auto i:mp)
-        {
-            if(i.second.size()>=2)
-            ans.push_back(i.second);
-        }
+        for(auto i:mp) if(i.second.size()>=2) ans.push_back(i.second);
         return ans;
     }
 };
