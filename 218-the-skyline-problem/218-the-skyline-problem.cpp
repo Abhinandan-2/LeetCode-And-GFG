4c0a4a7954acc472806591f8efee57fe74247b3f
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-10-01 01:11:11 +0530

    Time: 86 ms (25.38%), Space: 15.1 MB (61.01%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.cpp
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -1,84 +1,57 @@
 class Solution {
 public:
-    
-    static bool com(vector<int> &a,vector<int> &b)
-    {
-        if(a[0]==b[0])
-            return a[2]>b[2];
-        return a[0]<b[0];
-    }
-    
+    static bool com(vector<int> &a,vector<int> &b){ return a[0]==b[0] ? a[2]>b[2] : a[0]<b[0];}
     vector<vector<int>> getSkyline(vector<vector<int>>& a) {
-        
-        sort(a.begin(),a.end(),com);
         int n = a.size(),mx=0,mx1,x;
+        sort(a.begin(),a.end(),com);
         vector<vector<int>> ans;
         set<pair<int,int>> dis, high;
         pair<int,int> p;
-        for(int i=0;i<n;i++)
-        {
-            
-            if(dis.empty())
-                x = INT_MAX;
-            else
-                x = (*dis.begin()).first;
-            // cout<<i<<" "<<x<<endl;
-            if(a[i][0]<=x)
-            {
-                if(a[i][2]>mx)
-                {
+        for(int i=0;i<n;i++){
+            if(dis.empty()) x = INT_MAX;
+            else x = (*dis.begin()).first;
+            if(a[i][0]<=x){
+                if(a[i][2]>mx){
                     mx = a[i][2];
                     ans.push_back({a[i][0],mx});
                 }
                 dis.insert({a[i][1],a[i][2]});
                 high.insert({a[i][2],a[i][1]});
             }
-            else
-            {
+            else{
                 p = *dis.begin();
                 dis.erase(dis.begin());
                 high.erase({p.second,p.first});
-                if(dis.empty()||(*dis.begin()).first!=p.first)
+                if(dis.empty()||(*dis.begin()).first!=p.first&&p.second==mx)
                 {
-                    if(p.second==mx)
-                    {
-                        if(dis.empty())   
-                        {
-                            ans.push_back({p.first,0});
-                            mx =0; 
-                        }
-                        else{
-                            mx1 = (*high.rbegin()).first;
-                            if(mx!=mx1)
-                            {
-                                mx = mx1;
-                                ans.push_back({p.first,mx});
-                            }
+                    if(dis.empty())   {
+                        ans.push_back({p.first,0});
+                        mx =0; 
+                    }
+                    else{
+                        mx1 = (*high.rbegin()).first;
+                        if(mx!=mx1){
+                            mx = mx1;
+                            ans.push_back({p.first,mx});
                         }
                     }
                 }
                 i--;
             }
         }
-        
-        while(!dis.empty())
-        {
+        while(!dis.empty()){
             p = *dis.begin();
             dis.erase(dis.begin());
             high.erase({p.second,p.first});
-            if(dis.empty()||(*dis.begin()).first!=p.first)
-            {
-                if(p.second==mx)
-                {
-                    if(dis.empty())   
-                    {
+            if(dis.empty()||(*dis.begin()).first!=p.first){
+                if(p.second==mx){
+                    if(dis.empty())   {
                         ans.push_back({p.first,0});
                         mx =0; 
                     }
                     else{
                         mx1 = (*high.rbegin()).first;
-                        if(mx!=mx1)
-                        {
+                        if(mx!=mx1){
                             mx = mx1;
                             ans.push_back({p.first,mx});
                         }
