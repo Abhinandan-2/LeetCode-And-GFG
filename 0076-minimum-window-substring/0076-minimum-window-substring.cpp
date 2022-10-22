1dcaaaef32f7d882b2eabdd6f6f19f94f8e7b181
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-10-22 16:11:59 +0530

    Time: 198 ms (13.33%), Space: 7.8 MB (48.70%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    bool isGreater(unordered_map<char,int> &runingMap,unordered_map<char,int> &fixMap){
+        for(auto i:fixMap){
+            if(runingMap[i.first]<i.second) return 0;
+        }
+        return 1;
+    }
+public:
+    string minWindow(string a, string b) {
+        
+        int n = a.size() , m = b.size() ,mx= INT_MAX, pos = -1,i=0,j=0;
+        unordered_map<char,int> runingMap,fixMap;
+        for(auto i:b) fixMap[i]++;
+        while(i<n){
+            runingMap[a[i]]++;
+            if(isGreater(runingMap,fixMap)){
+                while(isGreater(runingMap,fixMap)){
+                    runingMap[a[j]]--;
+                    j++;
+                }
+                if(mx>i-j+2){
+                    mx = i-j+2;
+                    pos = i;
+                }
+            }
+            i++;
+        }
+        if(pos==-1) return "";
+        string ans = a.substr(pos-mx+1,mx);
+        return ans;
+    }
+};
