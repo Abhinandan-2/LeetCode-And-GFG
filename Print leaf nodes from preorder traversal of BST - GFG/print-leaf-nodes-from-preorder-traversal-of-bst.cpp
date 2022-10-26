5b052e36c50db6d4b81048feb7dddfbe3fb33654
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-10-27 01:20:08 +0530

    Added solution - LeetHub

--- Print leaf nodes from preorder traversal of BST - GFG/print-leaf-nodes-from-preorder-traversal-of-bst.cpp
+++ Print leaf nodes from preorder traversal of BST - GFG/print-leaf-nodes-from-preorder-traversal-of-bst.cpp
@@ -13,31 +13,22 @@ class Solution {
         vector<int> ans;
         stack<pair<int,int>> s;
         pair<int,int> p;
-        
         for(int i=0;i<n;i++){
             count =0;
-                while(!s.empty()&&a[i]>s.top().first){
-                    p = s.top();
-                    s.pop();
-                    if(p.second==0) ans.push_back(p.first);
-                    count++;
-                }
-                if(!s.empty()){
-                    p = s.top();
-                    s.pop();
-                    p.second=1;
-                    s.push(p);
-                }
-                s.push({a[i],0});
-                if(count==1) ans.pop_back();
-                // k = s;
-                // while(!k.empty()){
-                //     p = k.top();
-                //     k.pop();
-                //     cout<<p.first<<" "<<p.second<<" ";
-                // }
-                // cout<<endl;
-            
+            while(!s.empty()&&a[i]>s.top().first){
+                p = s.top();
+                s.pop();
+                if(p.second==0) ans.push_back(p.first);
+                count++;
+            }
+            if(!s.empty()){
+                p = s.top();
+                s.pop();
+                p.second=1;
+                s.push(p);
+            }
+            s.push({a[i],0});
+            if(count==1) ans.pop_back();
         }
         p = s.top();
         s.pop();
