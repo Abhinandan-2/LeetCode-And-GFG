d531847736a5d8316967058a40f9145a148fccd9
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-11-01 20:05:03 +0530

    Time: 47 ms (66.94%), Space: 13.3 MB (19.11%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,56 +1,37 @@
 class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& a) {
-        
         int n = a.size() , m = a[0].size(),i,j,f;
         vector<int> ans(m);
-        
-        for(int k=0;k<m;k++)
-        {
+        for(int k=0;k<m;k++){
             i =0 ;
-            j= k;
+            j = k;
             f =0;
-            while(i<n)
-            {
-                // cout<<i<<" "<<j<<" ";
-                if(a[i][j]==1)
-                {
-                    if(j<m-1&&a[i][j+1]==1)
-                    {
+            while(i<n){
+                if(a[i][j]==1){
+                    if(j<m-1&&a[i][j+1]==1){
                         i++;
                         j++;
                     }
-                    else
-                    {
+                    else{
                         f=1;
                         break;
                     }
                 }
-                else
-                {
-                    if(j>0&&a[i][j-1]==-1)
-                    {
+                else{
+                    if(j>0&&a[i][j-1]==-1){
                         i++;
                         j--;
                     }
-                    else
-                    {
+                    else{
                         f=1;
                         break;
                     }
                 }
-                // cout<<i<<" "<<j<<" ";
             }
-            // cout<<endl;
-            if(f==1)
-                ans[k] = -1;
-            else
-                ans[k] = j;
-            // cout<<ans[i]
+            if(f==1) ans[k] = -1;
+            else ans[k] = j;
         }
-        
-        return ans;
-        
-        
+        return ans;  
     }
 };
