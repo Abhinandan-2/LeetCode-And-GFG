89d44b5e034f7c8e03dff33a13fa4d6a0bb7429f
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-11-09 01:01:11 +0530

    Time: 8 ms (5.02%), Space: 6.2 MB (28.31%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -1,81 +1,45 @@
 class Solution {
 public:
     string fractionAddition(string a) {
-        vector<pair<int,int>> val;
-        vector<char> sign;
         int n = a.size(),i=0,sum=0,num,den,lcm,gcd,mainSign=0,mul;
-        string ans;
+        vector<pair<int,int>> val;
+        string ans,sign;
         if(a[0]!='-') sign.push_back('+');
         while(i<n){
-            if(a[i]=='-'||a[i]=='+'){
-                // cout<<i<<" "<<a[i]<<endl;
-                sign.push_back(a[i]);
-                i++;
-            }
+            if(a[i]=='-'||a[i]=='+') sign.push_back(a[i++]);
             else{
-                
                 num =0 ;
-                while(i<n&&a[i]!='+'&&a[i]!='-'&&a[i]!='/'){
-                    num *= 10;
-                    num += (a[i]-48);
-                    // cout<<i<<" "<<num<<" ";
-                    i++;
-                }
-                // cout<<num<<" ";
+                while(i<n&&a[i]!='+'&&a[i]!='-'&&a[i]!='/') {num *= 10;num += (a[i++]-48);}
                 i++;
                 den =0 ;
-                 while(i<n&&a[i]!='+'&&a[i]!='-'&&a[i]!='/'){
-                    den *= 10;
-                    den += (a[i]-48);
-                    // cout<<i<<" "<<den<<" ";
-                    i++;
-                }
-                // cout<<i<<endl;
+                while(i<n&&a[i]!='+'&&a[i]!='-'&&a[i]!='/') {den *= 10;den += (a[i++]-48);}
                 val.push_back({num,den});
             }
-            // cout<<i<<"->";
-            // for(auto i:sign) cout<<i<<" ";
-            // cout<<endl;
         }
-        // for(auto i:sign) cout<<i<<" ";
-        // cout<<endl;
-        // for(auto i:val) cout<<i.first<<" "<<i.second<<endl;
         if(sign.size()==1){
             gcd = __gcd(val[0].first,val[0].second);
             sum = val[0].first/gcd;
             lcm = val[0].second/gcd;
             if(sign[0]=='-') sum *=-1;
-            ans = to_string(sum);
-            ans += "/";
-            ans += to_string(lcm);
+            ans = to_string(sum)+"/"+to_string(lcm);
             return ans;
         }
         gcd = val[0].second;
         mul = val[0].second;
-        for(int i=1;i<val.size();i++){
-            gcd = __gcd(gcd,val[i].second);
-            mul *= val[i].second;
-        }
-        // cout<<gcd<<" "<<mul<<" ";
+        for(int i=1;i<val.size();i++) {gcd = __gcd(gcd,val[i].second);mul *= val[i].second;}
         lcm = mul/gcd;
-        sum =0 ;
         for(int i=0;i<val.size();i++){
             num = lcm/val[i].second;
             num *= val[i].first;
             if(sign[i]=='-') num*=-1;
             sum += num;
         }
-        if(sum<0){
-            mainSign =1 ;
-            sum *=-1;
-        }
+        if(sum<0) {mainSign =1;sum *=-1;}
         gcd = __gcd(sum,lcm);
         sum/=gcd;
         lcm/=gcd;
         if(mainSign==1) sum *=-1;
-        ans = to_string(sum);
-        ans += "/";
-        ans += to_string(lcm);
+        ans = to_string(sum)+"/"+to_string(lcm);
         return ans;
     }
 };
