0bfc1e27090904286d58ba49f7d51f9aedaa250f
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-11-17 18:47:08 +0530

    Added solution - LeetHub

---
+++ Count of Subarrays - GFG/count-of-subarrays.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	ll countSubarray(int a[], int n, int k) {
+	    
+	    ll ans =0,c=0 ;
+	    
+	    for(int i=0;i<n;i++){
+	        if(a[i]>k){
+	            c=0;
+	            ans += (n-i);
+	        }
+	        else{
+	            c++;
+	            ans += (n-i);
+	            ans -= c;
+	        }
+	    }
+	    
+	    return ans;
+	    
+	    
+	}
+};
+
+
+// 4 16
+// 30 4 23 6
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, i;
+        cin >> n >> k;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countSubarray(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
