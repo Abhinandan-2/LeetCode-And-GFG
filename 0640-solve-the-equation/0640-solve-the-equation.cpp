8c99f026e0ea9b44813ab24d29c0285a548f87fe
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-11-17 20:08:33 +0530

    Time: 2 ms (43.48%), Space: 6.1 MB (86.23%) - LeetHub

--- 0640-solve-the-equation/0640-solve-the-equation.cpp
+++ 0640-solve-the-equation/0640-solve-the-equation.cpp
@@ -6,35 +6,23 @@ public:
             if(a[i]=='-'||a[i]=='+'){
                 if(f==1) help *= -1;
                 l==0 ? lconst += help : rconst += help;
-                a[i]=='-' ? f=1 : f =0 ;
-                i++;
-                help =0;
-                noCoff =1;
+                a[i++]=='-' ? f=1 : f =0 ;
+                help =0; noCoff =1;
             }
             else if(a[i]>=48&&a[i]<=57){
-                help=0;
-                noCoff = 0;
+                help=0; noCoff = 0;
                 while(i<n&&a[i]>=48&&a[i]<=57) help = (help*10)+(a[i++]-48);
             }
             else if(a[i]=='='){
-                if(f==0) lconst += help; 
-                else lconst -= help;
-                l=1;
-                i++;
-                f=0;
-                help=0;
-                noCoff =1;
+                f==0 ? lconst += help : lconst -= help;
+                l=1; i++; f=0; help=0; noCoff =1;
             }
             else if(a[i]=='x'){
                 l==0 ? noCoff==1 ? f==0 ? lcoff++ : lcoff-- : f==0 ? lcoff+= help : lcoff -= help : noCoff==1 ? f==0 ? rcoff++ : rcoff-- : f==0 ? rcoff+= help : rcoff -= help;
-                help=0;
-                f=0;
-                i++;
-                noCoff = 1;
+                help=0; f=0; i++; noCoff = 1;
             }
         }
-        if(f==0) rconst += help;
-        else rconst -= help;
+        f==0 ? rconst += help : rconst -= help;
         if(lcoff-rcoff==0) return rconst-lconst==0 ? "Infinite solutions" : "No solution";
         return "x=" + to_string((rconst-lconst)/(lcoff-rcoff));
     }
