5007a8ad82f12d27cf6b94915792e64c0057b742
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2022-11-19 20:28:41 +0530

    Time: 538 ms (5.60%), Space: 74.7 MB (5.60%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.cpp
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -6,47 +6,15 @@ public:
         double help;
         for(int i=0;i<a.size();i++) a[i][0]==p.first ? (f==2||f==4) ? slope.push_back(-1000.0) : slope.push_back(1000.0) : slope.push_back((double)(a[i][1]-p.second)/(double)(a[i][0]-p.first));
         help = 1001.0;
-        
         for(int i=0;i<slope.size();i++){
             if(slope[i]<=help){
                 if(slope[i]==help){
-                    if(f==1){
-                        if(a[i][0]>ans.first){
-                            ans.first = a[i][0];
-                            ans.second = a[i][1];
-                        }
-                    }
-                    else if(f==2){
-                        if(a[i][1]>ans.second){
-                            ans.first = a[i][0];
-                            ans.second = a[i][1];
-                        }
-                    }
-                    else if(f==3){
-                        if(a[i][0]<ans.first){
-                            ans.first = a[i][0];
-                            ans.second = a[i][1];
-                        }
-                    }
-                    else if(f==4){
-                        if(a[i][1]<ans.second){
-                            ans.first = a[i][0];
-                            ans.second = a[i][1];
-                        }
-                    }
-                }
-                else{
-                    help = slope[i];
-                    ans.first = a[i][0];
-                    ans.second = a[i][1];
-                }
+                    if((f==1&&a[i][0]>ans.first)||(f==2&&a[i][1]>ans.second)||(f==3&&a[i][0]<ans.first)||(f==4&&a[i][1]<ans.second)){ans.first = a[i][0];ans.second = a[i][1];}}
+                else{help = slope[i];ans.first = a[i][0];ans.second = a[i][1];}
             }
         }
-        
         return ans;
-        
     }
-    
     pair<int,int> mainAlgo(pair<int,int> &point,set<vector<int>> &ans,int f,vector<vector<int>> a){
         vector<vector<int>> allWorthy;
         while(1){
@@ -59,21 +27,13 @@ public:
         }
         return point;
     }
-    
     vector<vector<int>> outerTrees(vector<vector<int>>& a) {
         int n = a.size()  ;
         if(n==1) return a;
         pair<int,int> topRight = {INT_MIN,INT_MIN},leftTop,downLeft,rightDown;
         vector<vector<int>> ans;
         set<vector<int>> res;
-        for(int i=0;i<n;i++){
-            if(a[i][1]>=topRight.second){
-                if(a[i][0]>=topRight.first){
-                    topRight.second = a[i][1];
-                    topRight.first = a[i][0];
-                }
-           }
-        }
+        for(int i=0;i<n;i++) if(a[i][1]>=topRight.second) if(a[i][0]>=topRight.first){topRight.second = a[i][1];topRight.first = a[i][0];}
         leftTop = mainAlgo(topRight,res,1,a);
         downLeft = mainAlgo(leftTop,res,2,a);
         rightDown = mainAlgo(downLeft,res,3,a);
