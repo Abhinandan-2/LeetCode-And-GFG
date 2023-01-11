6741b19b37eb9e137c6f13df9baa00865da460a6
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2023-01-11 23:23:00 +0530

    Added solution - LeetHub

---
+++ Make array elements unique - GFG/make-array-elements-unique.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minIncrements(vector<int> a, int n) {
+        int count =1 , extra =0 , gap, diff ;
+        long long int ans =0 ;
+        sort(a.begin(),a.end());
+        for(int i=1;i<n;i++){
+            if(a[i]==a[i-1]) count++;
+            else{
+                extra += count-1;
+                gap = a[i]-a[i-1]-1;
+                if(gap==0) ans += extra;
+                else if(gap>=extra){
+                    ans += ((long long int)extra*(extra+1))/2;
+                    extra =0 ;
+                }
+                else{
+                    diff = extra-gap;
+                    ans +=  ((long long int)extra*(extra+1))/2;
+                    ans -=  ((long long int)diff*(diff+1))/2;
+                    extra -= gap;
+                    ans += extra;
+                    // ans -= ((long long int)(extra-gap)*(extra-gap+1))/2;
+                } 
+                count =1 ;
+            }
+        }
+        extra += count-1;
+        // cout<<extra ;
+        ans += ((long long int)extra*(extra+1))/2;
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minIncrements(arr, N) << endl;
+    }
+}
+// } Driver Code Ends
