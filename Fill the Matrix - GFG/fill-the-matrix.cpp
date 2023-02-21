aa2221ca593ad367157dbd761e75067bfe78f898
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2023-02-21 15:08:10 +0530

    Added solution - LeetHub

---
+++ Fill the Matrix - GFG/fill-the-matrix.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int minIteration(int n, int m, int x, int y){    
+        int first = (y-1)+(x-1);
+        int second = (y-1)+(n-x);
+        int third = (m-y)+(x-1);
+        int fourth = (m-y)+(n-x);
+        return max({first,second,third,fourth});
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, M, x, y;
+        cin >> N >> M;
+        cin >> x >> y;
+        
+        Solution ob;
+        cout << ob.minIteration(N, M, x, y) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
