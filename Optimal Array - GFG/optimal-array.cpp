2e23a48ba710bde64bb37101cfdd063c8d320244
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2023-02-28 16:18:52 +0530

    Added solution - LeetHub

--- Optimal Array - GFG/optimal-array.cpp
+++ Optimal Array - GFG/optimal-array.cpp
@@ -12,10 +12,7 @@ class Solution{
 public:
     vector<int> optimalArray(int n,vector<int> &a){
         vector<int> ans(n,0);
-        for(int i=1;i<n;i++){
-            // if(i%2==0) j++;
-            ans[i]=ans[i-1]+a[i]-a[i/2];
-        }
+        for(int i=1;i<n;i++) ans[i]=ans[i-1]+a[i]-a[i/2];
         return ans;
     }
 };
