1da624cf8fd3374751ef436a5bcc4d98507193c5
Abhinandan <57489839+Abhinandan-2@users.noreply.github.com>
2023-05-14 15:18:54 +0530

    Added solution - LeetHub

--- Maximum subset sum - GFG/maximum-subset-sum.cpp
+++ Maximum subset sum - GFG/maximum-subset-sum.cpp
@@ -29,7 +29,6 @@ public:
 
 // } Driver Code Ends
 class Solution {
-
   public:
     long long findMaxSubsetSum(int n, vector<int> &a) {
         vector<long long> ans(n+1,0);
@@ -37,26 +36,6 @@ class Solution {
         for(int i=2;i<=n;i++) ans[i] = max(ans[i-1],ans[i-2])+a[i-1];
         return max(ans[n],ans[n-1]);
     }
-    
-    /*
-    
-    long long findMaxSubsetSum(int n, vector<int> &a) {
-        long long sum =0 , i = 0 ;
-        if(n==1){
-            if(a[0]>=0) return a[0];
-            return 0;
-        }
-        if(a[0]<0&&a[1]<0) i = a[0]>=a[1] ? 0 :  1;
-        else if(a[0]<0) i = 1;
-        while(i<n){
-            sum += a[i];
-            if(i+1<n&&a[i]>=0) i++;
-            else if(i+2<n&&)
-        }
-        return 0;
-    }
-    
-    */
 };
 
 
